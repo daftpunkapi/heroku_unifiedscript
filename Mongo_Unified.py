2b3c466a5eec6ac8a90715ce65ad578197da9180
Raghav Mehta <49563157+raghavm23@users.noreply.github.com>
2022-11-24 13:28:39 +0530

    Pagination

--- Mongo_Unified.py
+++ Mongo_Unified.py
@@ -1,7 +1,8 @@
 # Load Dependencies/Libraries
-from flask import Flask, request, json, Response
-from pymongo import MongoClient
-
+from flask import Flask, request, json, Response, jsonify
+from pymongo import MongoClient 
+import pymongo
+# import pandas as pd
 
 # Create Connect with MongoDB
 class MongoAPI:
@@ -11,50 +12,60 @@ class MongoAPI:
         collection = data
         cursor = self.client[database]
         self.collection = cursor[collection]
-        # self.data = data
 
-
-
-# This method will allow us to read all of the documents present in our collection. Line number 3 is used to reformat the data. The output of the collection object is of datatype dictionary
+# This method will fetch the documents from the collection that match the params criteria
+# The requested documents are also paginated
     def read(self,params):
-        # status = params["status"]
-        # pagesize = params["pages"]
         
-        if "status" in params:
-            documents = self.collection.find({"status" : params["status"]})
-            output = [{item: data[item] for item in data if item != '_id'} for data in documents] #removing the MongoDB generated ID
-            return output
+        page_size = 5
+        offset = 0
+       
+        if "page_size" in params:
+            page_size = int(params["page_size"])
+            del params["page_size"] 
         else:
-            documents = self.collection.find()
-            output = [{item: data[item] for item in data if item != '_id'} for data in documents] #removing the MongoDB generated ID
-            return output
-
-
-
+            pass
+        
+        if "offset" in params:
+            offset = int(params["offset"])
+            del params["offset"]
+        else:
+            pass
+    
+        # cursor pointing to only ONE document which is 'offset' element of the filtered and sorted collection
+        last_id = self.collection.find(params).sort('_id', 1)[offset] 
+        
+        # finding the documents that match our parameter criteria and is greater than ObjectID of last_id
+        # also '_id':0 ensures that ObectId is not returned in the document query
+        # documents are limited as per the page size
+        documents = self.collection.find({'$and': [params,{'_id': {'$gte' : last_id['_id']}}]},{'_id':0}).sort('_id', 1).limit(page_size)
+        
+        output =[]
+        for i in documents:
+            output.append(i)
+    
+        next_url = '/tickets?page_size=' + str(page_size) + '&offset=' + str(offset + page_size)
+        prev_url = '/tickets?page_size=' + str(page_size) + '&offset=' + str(offset - page_size)
+        
+        return jsonify({'prev_url': prev_url, 'next_url': next_url, 'result': output})
+    
 # Create a Flask Server
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
-
-
 
 # HTTP Method via Flask Routes
 @app.route('/tickets', methods=['GET'])
 def mongo_read():
     data = request.headers['Account-Token']
-    # status = request.args.get('status')
     params = request.args
     params = params.to_dict(flat = True)  
                         
     obj1 = MongoAPI(data)
     response = obj1.read(params)
-    return Response(response=json.dumps(response),
-                    status=200,
-                    mimetype='application/json')
+    return response
 
 # Running the Flask Server/App
 if __name__ == '__main__':
-    # app.run(debug=True, host='0.0.0.0', port=5001)
+    # app.run(debug=True, host='0.0.0.0', port=5003)
     from os import environ
     app.run(debug=True, host='0.0.0.0', port=environ.get("PORT", 5000))
-   
-
